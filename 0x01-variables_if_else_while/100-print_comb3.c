398be7f2fbf32b94896d28737c987d65d0a25847
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-02-20 15:36:40 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (digit1 = '0'; digit1 < '9'; digit1++)
 	{
-		for (digit2 = '0'; digit2 < '9'; digit2++)
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
 		{
 			if (digit1 < digit2)
 			{
 				putchar(digit1);
 				putchar(digit2);
 
-				if (digit1 != '8' || (digit1 == '8' && digit2 != '9'))
+				if (digit1 != '8' || (digit1 != '8' && digit2 != '9'))
 				{
 					putchar(',');
 					putchar(' ');
