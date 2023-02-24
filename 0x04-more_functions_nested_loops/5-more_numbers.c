01ea381df79b0e760f85fd4882b6ec87567f2858
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-02-24 13:57:10 -0800

    ...

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - multipe lines
+ *
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar(i);
+			_putchar(x % 10 + '0');
+		}
+			_putchar('\n');
+	}
+}
+
