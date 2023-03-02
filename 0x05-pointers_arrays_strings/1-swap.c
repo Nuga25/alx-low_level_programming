1388a9d9f9b52be3a9e37df715b7c2dfa81320d9
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-03-02 08:00:44 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps int variables
+ * @a: int to check
+ * @b: int to check
+ * Return: swap_int
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
