3a463bf43d9b68e72ab28bb4a68ce1733dab58df
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-03-15 09:09:24 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an arrary of char
+ * @size: size of array
+ * @c: arrray initialization
+ *
+ * Return: myArray
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *myArray;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	myArray = (char *)malloc(size * sizeof(char));
+
+	if (myArray == NULL)
+		return (NULL)
