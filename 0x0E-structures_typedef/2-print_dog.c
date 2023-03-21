bd9f6055c30730d9076381bba1ef618cd6454c16
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-03-21 08:21:54 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,15 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("nil");
+		else
+			printf("Name: %s\n", d->name);
 		if (d->age == NULL)
 			printf("nil");
+		else
+			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("nil");
 		else
-			printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
