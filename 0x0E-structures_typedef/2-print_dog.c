d09fab5d6f8e424c9030ebeb02efbc7f8f920ed4
Osinuga Ifeoluwa <osinugaifeoluwa1@gmail.com>
2023-03-21 07:56:32 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints dog class
+ * @d: struct variable
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("nil");
+	if (d->age == NULL)
+		printf("nil");
+	if (d->owner == NULL)
+		printf("nil");
+
+	if (d == NULL)
+		return (NULL);
+
+	if (d)
+	{
+		printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
